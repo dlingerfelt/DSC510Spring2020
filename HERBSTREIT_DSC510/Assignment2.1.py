3dd698161207c598fe8d755f2547dc758a63b69d
sherbstreit <sherbstreit@my365.bellevue.edu>
2020-03-23 16:51:02 -0500

    Assignment2.1

---
+++ HERBSTREIT_DSC510/Assignment2.1.py
@@ -0,0 +1,63 @@
+#%%
+# File: Assignment_2_1.py
+# Sara Herbstreit
+# 03/22/20
+# DSC510-T303 Introduction to Programming (2203-1)
+
+# Retrieve Company name
+company = input('Welcome! Which company are you shopping for?\n')
+# Retrieve number of feet desired.
+feet = input('How many feet of fiber optic cable do you need?\n')
+# Convert string to integer
+feet = int(feet)
+# Create variable for price
+price = 0.87
+# Convert feet integer to float
+# Determine cost by multiplying feet by price
+cost = float(feet) * (price)
+# Display total cost
+print('For this much fiber optic cable it will cost:')
+print (cost)
+# Display receipt
+print('Receipt')
+# Display company name
+print(company)
+# Display feet requested
+print('Feet requested')
+print(feet)
+# Display price per foot
+print('Price per foot')
+print(price)
+# Display total cost
+print('Total cost')
+print(cost)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
