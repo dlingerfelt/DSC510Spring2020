020910bba36597cc5ae6a2c5b2aac08c7dcf10bb
sherbstreit <sherbstreit@my365.bellevue.edu>
2020-03-28 17:41:08 -0500

    Assignment 3.1

--- HERBSTREIT_DSC510/Assignment 3.1.py
+++ HERBSTREIT_DSC510/Assignment 3.1.py
@@ -5,14 +5,17 @@
 
 # Retrieve Company name
 company = input('Welcome! Which company are you shopping for?\n')
+
 # Tell about discounts
 print('Bulk discount can apply')
 print('500+ feet is $0.50/foot')
 print('251-499 feet is $0.70/foot')
 print('101-250 feet is $0.80/foot')
 print('Under 100 feet is $0.87/foot')
+
 # Retrieve number of feet desired.
 feet = int(input('How many feet of fiber optic cable do you need?\n'))
+
 # Create variable for price
 if feet >= 500:
     price = 0.50
@@ -22,16 +25,21 @@ elif feet >= 100:
     price = 0.80
 else:
     price = 0.87
+
 # Multiply feet by price
 cost = int(price * feet)
+
 # Convert price to $x.xx
 price = '{0:.2f}'.format(price)
 price = '$' + str(price)
+
 # Convert cost to $x.xx
 cost = '{0:.2f}'.format(cost)
 cost = 'The total cost is $' + str(cost)
+
 # Display total cost
 print(cost)
+
 # Display receipt
 print('Receipt')
 # Display company name
