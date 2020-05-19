97837ee1e36cd2717ffc07683a05156103aef3e4
htran017 <61989117+htran017@users.noreply.github.com>
2020-05-19 10:40:24 -0700

    Update wk10_API_ChuckNorrisJokes.py

--- TRAN_DSC510/wk10_API_ChuckNorrisJokes.py
+++ TRAN_DSC510/wk10_API_ChuckNorrisJokes.py
@@ -9,10 +9,11 @@
 # Display to user the Data associated with value key
 # Allow user to request as many Chuck Norris jokes as they want
 
-
+# installed the requests library
 import requests
 import json
 
+# function to receive a json response and parse json
 def service_joke():
     serviceurl = 'https://api.chucknorris.io/jokes/random'
     try:
@@ -21,10 +22,14 @@ def service_joke():
         raise Exception(f'==== Fail to Retrieve ===={fail}')
     return joke
 
+# created a pretty print function so only the value is displayed for user
 def pretty(joke):
     print('--------This is your joke----------')
     print(joke['value'])
-
+    
+# created an error checking function so if user enters various forms of Yes
+# the joke will display and forms of No will exit program
+# anything that isn't Y/N will be an invalid user response
 def user_inp():
     user = input('Are you ready for a Chuck Norris joke? Yes or No:\n')
     if user == 'Yes' or user == 'Y' or user == 'y':
@@ -36,8 +41,11 @@ def user_inp():
     else:
         print('==== Invalid Response: Failure to Retrieve ====')
         exit()
+       
 def main():
+     # Display Welcome message for user
     print('Welcome to the World that is Chuck Norris!')
+    # the while loop allows user to request as many jokes as they want
     while True:
         (user_inp())
         pretty(service_joke())
